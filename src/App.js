4e705124aa00d0b06a8d2fb465b1cf2e4e984390
hellowurld-create <akpagodswill2@outlook.com>
2022-12-07 19:07:53 +0100

    start page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
@@ -7,17 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Move Down👇🏽
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
