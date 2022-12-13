796848e4ca6f71cc5012e4c3c7bd16365ce77750
hellowurld-create <akpagodswill2@outlook.com>
2022-12-13 14:46:06 +0100

    made some changes

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './ExpenseForm'
+
+const ExpenseForm = () => {
+    return <form>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+        </div>
+        </div>
+    </form>
+}
+
+export default ExpenseForm;
